53da14f726fa409c2e9af503c2a44bbd9eb1071f
Dimitar Ivanov <biggie@abv.bg>
2019-07-09 08:03:26 +0300

    Rename files

--- src/HeadCouch.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "HeadCouchException.php";
-require_once "HeadCouchCurl.php";
-require_once "HeadCouchSocket.php";
-require_once "HeadCouchServer.php";
-require_once "HeadCouchDatabase.php";
-require_once "HeadCouchDocument.php";
-require_once "HeadCouchDdoc.php";
-?>
